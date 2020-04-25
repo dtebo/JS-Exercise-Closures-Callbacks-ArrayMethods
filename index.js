f1b831f1ff161e6ed39bd6e42c08e8d883771df1
Darren Tebo <darrentebo83@outlook.com>
2020-04-25 15:00:30 -0400

    passed twelve tests

--- index.js
+++ index.js
@@ -236,8 +236,12 @@ function stringSmash(strings) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  const fullNames = [];
+
+  runners.forEach((runner) => { return fullNames.push(`${runner.last_name}, ${runner.first_name}`); });
+
+  return fullNames.sort();
 }
 
 /**
