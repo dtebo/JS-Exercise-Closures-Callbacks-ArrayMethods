8a3709b0453bc890b8827c295712087c4aa0b181
Darren Tebo <darrentebo83@outlook.com>
2020-04-25 15:14:52 -0400

    passed fifteen tests

--- index.js
+++ index.js
@@ -241,7 +241,7 @@ function getFullNames(runners) {
 
   runners.forEach((runner) => { return fullNames.push(`${runner.last_name}, ${runner.first_name}`); });
 
-  return fullNames.sort();
+  return fullNames;
 }
 
 /**
@@ -256,8 +256,8 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  return runners.map((item) => { return item.first_name.replace(/[a-zA-Z]/, l => l.toUpperCase()); });
 }
 
 /**
